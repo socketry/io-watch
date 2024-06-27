a4d5581f322fe3889347b97c9aa9fad7b7d7d10b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-27 18:42:24 +0900

    Add support for recursive monitoring.

--- test/io/monitor/filesystem.rb
+++ test/io/monitor/filesystem.rb
@@ -31,4 +31,30 @@ describe IO::Monitor::Filesystem do
 		event = changes.pop
 		expect(event[:path]).to be == root
 	end
+	
+	it 'should detect a new directory and changes within it' do
+		changes = Thread::Queue.new
+		
+		thread = Thread.new do
+			monitor = IO::Monitor::Filesystem.new([root])
+			monitor.run do |event|
+				changes << event
+			end
+		end
+		
+		# Wait for the monitor to start...
+		changes.pop
+		
+		test_dir = File.join(root, 'test')
+		Dir.mkdir(test_dir)
+		
+		event = changes.pop
+		expect(event[:path]).to be == root
+		
+		test_file = File.join(test_dir, 'test.txt')
+		File.write(test_file, 'hello')
+		
+		event = changes.pop
+		expect(event[:path]).to be == root
+	end
 end
