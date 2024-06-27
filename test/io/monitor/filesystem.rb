c69942c69152e114d56596a473b3c1c95e4c14a3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-27 22:43:48 +0900

    Support directory removals. (#2)

--- test/io/monitor/filesystem.rb
+++ test/io/monitor/filesystem.rb
@@ -57,4 +57,29 @@ describe IO::Monitor::Filesystem do
 		event = changes.pop
 		expect(event[:path]).to be == root
 	end
+	
+	it "should detect directory additions and removals" do
+		changes = Thread::Queue.new
+		
+		thread = Thread.new do
+			monitor = IO::Monitor::Filesystem.new([root])
+			monitor.run do |event|
+				changes << event
+			end
+		end
+		
+		# Wait for the monitor to start...
+		changes.pop
+		
+		test_dir = File.join(root, 'test')
+		Dir.mkdir(test_dir)
+		
+		event = changes.pop
+		expect(event[:path]).to be == root
+		
+		Dir.rmdir(test_dir)
+		
+		event = changes.pop
+		expect(event[:path]).to be == root
+	end
 end
